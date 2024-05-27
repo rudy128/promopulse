6da21f7b60b1522906e9d6de39a1c6d63b7a3a76
Rudy <prathamkamthan1306@gmail.com>
2024-05-27 15:59:43 +0530

    Added ribbons with responsiveness

--- app/page.tsx
+++ app/page.tsx
@@ -14,17 +14,17 @@ export default function Home() {
       </div>
 
       <Hero />
-      <div className="bg-[#fff] text-black p-16 w-full flex flex-col justify-center items-center">
-        <h1 className="font-bold text-4xl md:text-5xl lg:text-6xl">Are you him/ Are you her?</h1>
-        <div className="lg:flex lg:space-x-5 space-y-6 justify-around w-full p-6 items-center">
-          <div className="border text-2xl rounded-xl shadow-md p-6">
-            <h1 className="p-2 text-3xl font-semibold">Let<span>'</span>s get to know each other if:</h1>
+      <div className="bg-[#fff] relative text-black p-16 w-full flex flex-col justify-center items-center">
+        <h1 className="font-bold text-4xl md:text-5xl">We don<span>'</span>t want you to make a mistake now!</h1>
+        <div className="lg:flex lg:space-x-5 space-y-6 justify-start h-[40dvh] w-full p-6 items-start">
+          <div className="lg:inset-[150px_52.5dvw_auto_auto] sm:w-[65%] lg:w-1/2 lg:pr-32 lg:pl-40 lg:pb-28 ribbon-left bg-black text-white lg:text-lg p-6 sm:text-[0.7em] sm:pb-16 sm:pl-20 sm:inset-[18dvh_41dvw_auto_auto] text-[0.6em] pr-12 w-[80%] pl-8 pb-12 pt-2 inset-[25dvh_25dvw_auto_auto]">
+            <h1 className="p-2 lg:text-3xl font-semibold">Let<span>'</span>s get to know each other if:</h1>
             <li>You<span>'</span>re willing to change your income.</li>
             <li>You<span>'</span>re looking for more time away from work</li>
             <li>You have a problem that you want solved (if you know it or not)</li>
           </div>
-          <div className="border text-2xl shadow-md rounded-xl p-6">
-            <h1 className="p-2 text-3xl font-semibold">Let<span>'</span>s not go down this road if:</h1>
+          <div className="lg:inset-[300px_-7dvw_auto_auto] md:inset-[30dvh_-3dvw_auto_auto] lg:w-1/2 lg:pl-52 lg:pb-28 md:pl-16 md:pb-20 ribbon-right sm:pb-16 sm:pl-16 sm:inset-[50dvw_-1.5dvw_auto_auto] sm:w-[55%] sm:text-[0.7em] md:text-sm lg:text-lg p-6 bg-black text-white text-[0.5em] w-[75%] pb-12 pl-9 pt-3 sm:pt-0 inset-[45dvh_-4dvw_auto_auto]">
+            <h1 className="p-2 lg:text-3xl font-semibold">Let<span>'</span>s not go down this road if:</h1>
             <li>You aren<span>'</span>t willing to provide data about your business</li>
             <li>You<span>'</span>re looking for temporary solutions</li>
             <li>You<span>'</span>re unwilling to collaborate</li>
